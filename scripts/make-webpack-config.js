4445aaccd3cca231b3a5baf28699ed0389ed85f2
konstantin krivlenia <krivlenia@gmail.com>
2018-03-05 12:13:12 +0300

    remove uglify plugins

--- scripts/make-webpack-config.js
+++ scripts/make-webpack-config.js
@@ -43,7 +43,6 @@ module.exports = function(config, env) {
 				inject: true,
 			}),
 			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 				'process.env.STYLEGUIDIST_ENV': JSON.stringify(env),
 			}),
 		],
@@ -63,21 +62,6 @@ module.exports = function(config, env) {
 				chunkFilename: 'build/[name].[chunkhash:8].js',
 			},
 			plugins: [
-				new UglifyJSPlugin({
-					parallel: true,
-					cache: true,
-					uglifyOptions: {
-						ie8: false,
-						ecma: 5,
-						compress: {
-							keep_fnames: true,
-							warnings: false,
-						},
-						mangle: {
-							keep_fnames: true,
-						},
-					},
-				}),
 				new CleanWebpackPlugin(['build'], {
 					root: config.styleguideDir,
 					verbose: config.verbose === true,
